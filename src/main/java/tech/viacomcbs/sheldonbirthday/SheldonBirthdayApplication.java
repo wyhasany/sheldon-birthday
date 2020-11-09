7d6df0cc5069aa2edc47e000cdf4c9cb04eeb722
Michal Rowicki <Michal.Rowicki@vimn.com>
2020-11-09 10:20:49 +0100

    init

---
+++ src/main/java/tech/viacomcbs/sheldonbirthday/SheldonBirthdayApplication.java
@@ -0,0 +1,14 @@
+package tech.viacomcbs.sheldonbirthday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+public class SheldonBirthdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheldonBirthdayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/viacomcbs/sheldonbirthday/SheldonBirthdayApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.viacomcbs.sheldonbirthday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheldonBirthdayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
